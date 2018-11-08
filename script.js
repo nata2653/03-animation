ea9089ffdc89d4a170df1bf41e9428d9e8d2f081
Natalie Sirich <nataliesirich@Natalies-MacBook-Pro.local>
2018-11-08 10:21:39 +0100

    JS random - virker

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let count = 0;
-let mist = 1;
-let timeLeft = 20;
+let mist = 3;
+let timeLeft = 200;
+let time = 30;
+let showSettingsEffektSound = true;
+let timeLeftTimeOut;
+
 
 window.addEventListener("load", sidenVises);
 
@@ -12,6 +16,11 @@ function sidenVises() {
     document.querySelector("#count").innerHTML = count;
 
     document.querySelector("#mist").innerHTML = mist;
+
+    document.querySelector("#time").innerHTML = "00: " +
+        time;
+
+
 }
 
 function showStart() {
@@ -24,6 +33,37 @@ function showStart() {
 
     document.querySelector("#game_elements").classList.add("hide");
 
+    document.querySelector("#settings").addEventListener("click", showSettings);
+
+    document.querySelector("#time").classList.add("timeTell");
+
+    document.querySelector("#musik").play();
+
+
+}
+
+function timeTelling() {
+    console.log("TimeTell");
+
+    setTimeout(timeLeftFc, 1000);
+
+}
+
+function showSettings() {
+    console.log("showSettings");
+    document.querySelector("#settings_screen").classList.remove("hide");
+    document.querySelector("#setting_music").addEventListener("click", toggleMusic);
+    document.querySelector("#setting_effekt_sound").addEventListener("click", toggleSounds);
+    document.querySelector("#setting_close").addEventListener("click", hideSettings);
+}
+
+function hideSettings() {
+    console.log("hideSettings");
+
+    document.querySelector("#settings_screen").classList.add("hide");
+    document.querySelector("#setting_close").removeEventListener("click", hideSettings);
+    document.querySelector("#setting_music").removeEventListener("click", toggleMusic);
+    document.querySelector("#setting_effekt_sound").removeEventListener("click", toggleSounds);
 }
 
 function hideStart() {
@@ -47,62 +87,141 @@ function startGame() {
     document.querySelector("#start").removeEventListener("animationend", startGame);
 
     document.querySelector("#start").classList.add("hide");
-
     //Elementer
-    //document.querySelector("#paraply").addEventListener("click", paraplyClick);
+    document.querySelector("#paraply").addEventListener("click", paraplyClick);
+    document.querySelector("#paraply").classList.add("paraply_ned");
 
-    //paraply();
+
     document.querySelector("#reset").addEventListener("click", tabtLiv);
 
     document.querySelector("#sur_sky").addEventListener("click", surSkyClick);
+    document.querySelector("#sur_sky").classList.add("sky_ned");
+
 
     document.querySelector("#sol").addEventListener("click", solClick);
+    document.querySelector("#sol").classList.add("sol_ned");
+
     document.querySelector("#solbriller").addEventListener("click", solbrillerClick);
+    document.querySelector("#solbriller").classList.add("solbriller_ned");
+
 
     document.querySelector("#is").addEventListener("click", isClick);
+    document.querySelector("#is").classList.add("is_ned");
+
+
+    document.querySelector("#time").addEventListener("click", timeTelling);
 
     if (document.querySelector("#sol").classList.contains("hide")) {
         document.querySelector("#sol").classList.remove("hide")
     }
-
+    setTimeout(solFrem, 1000);
+    setTimeout(solbrillerFrem, 1000);
+    setTimeout(isFrem, 1000);
+    setTimeout(skyFrem, 1000);
+    setTimeout(paraplyFrem, 1000);
 
 }
 
-//function paraply() {
-//    console.log("paraply_kaldt")
-//    document.querySelector("#paraply").addEventListener("click", paraplyClick);
-//
-//    //document.querySelector("#paraply").classList.remove("hide");
-//
-//}
+function solFrem() {
+    console.log("Sol Frem");
+    if (document.querySelector("#sol").classList.contains("sol_ned")) {
+
+    } else {
+        document.querySelector("#sol").className = "";
+        document.querySelector("#sol").classList.add("sol_ned");
+        document.querySelector("#sol").classList.add("position" + Math.floor((Math.random() * 5) + 1));
+    }
+    setTimeout(solFrem, 1000);
+    document.querySelector("#sol").addEventListener("click", solClick);
+
+}
+
+function solbrillerFrem() {
+    console.log("Solbriller Frem");
+    if (document.querySelector("#solbriller").classList.contains("solbriller_ned")) {
+
+    } else {
+        document.querySelector("#solbriller").className = "";
+        document.querySelector("#solbriller").classList.add("sol_ned");
+        document.querySelector("#solbriller").classList.add("position" + Math.floor((Math.random() * 5) + 1));
+    }
+    setTimeout(solbrillerFrem, 1000);
+    document.querySelector("#sol").addEventListener("click", solClick);
+
+}
+
+function isFrem() {
+    console.log("is Frem");
+    if (document.querySelector("#is").classList.contains("is_ned")) {
+
+    } else {
+        document.querySelector("#is").className = "";
+        document.querySelector("#is").classList.add("sol_ned");
+        document.querySelector("#is").classList.add("position" + Math.floor((Math.random() * 5) + 1));
+    }
+    setTimeout(isFrem, 1000);
+    document.querySelector("#is").addEventListener("click", solClick);
+
+}
+
+function skyFrem() {
+    console.log("Sky Frem");
+    if (document.querySelector("#sur_sky").classList.contains("sky_ned")) {
+
+    } else {
+        document.querySelector("#sur_sky").className = "";
+        document.querySelector("#sur_sky").classList.add("sol_ned");
+        document.querySelector("#sur_sky").classList.add("position" + Math.floor((Math.random() * 5) + 1));
+    }
+    setTimeout(skyFrem, 1000);
+    document.querySelector("#sur_sky").addEventListener("click", solClick);
+
+}
+
+function paraplyFrem() {
+    console.log("Paraply Frem");
+    if (document.querySelector("#paraply").classList.contains("paraply_ned")) {
+
+    } else {
+        document.querySelector("#paraply").className = "";
+        document.querySelector("#paraply").classList.add("sol_ned");
+        document.querySelector("#paraply").classList.add("position" + Math.floor((Math.random() * 5) + 1));
+    }
+    setTimeout(paraplyFrem, 1000);
+    document.querySelector("#paraply").addEventListener("click", solClick);
+
+}
+
+
+//          KODE FOR ELEMENTER DER FALDER NED!
+
 
 function paraplyClick() {
     console.log("minus_point");
     console.log("minus_mist")
     //point = point + 1;
-    count = count - 5;
+    count = count - 1;
     //point++; fortæller det samme som point = point +1;
     mist = mist - 1;
-
-    if (mist < 1) {
-        gameover();
-    }
 
     console.log(count);
 
     document.querySelector("#count").innerHTML = count;
 
     console.log(this);
-    this.classList.add("hide");
+    //this.classList.add("hide");
+    this.classList.remove("paraply_ned");
+
     this.removeEventListener("click", paraplyClick);
 
+    document.querySelector("#buzzLyd").play();
 
     console.log(mist);
 
     document.querySelector("#mist").innerHTML = mist;
-    document.querySelector("#paraply").remove();
+    gameStatus();
+    tabtPoint();
 
-    //paraply();
 }
 
 
@@ -110,23 +229,27 @@ function surSkyClick() {
     console.log("minus_point");
     console.log("minus_mist")
     //point = point + 1;
-    count = count - 5;
+    count = count - 1;
+    //point++; fortæller det samme som point = point +1;
     mist = mist - 1;
 
-    tabtLiv();
-
-    //point++; fortæller det samme som point = point +1;
     console.log(count);
 
     document.querySelector("#count").innerHTML = count;
 
     console.log(this);
-    this.classList.add("hide");
+    //this.classList.add("hide");
+    this.classList.remove("sky_ned");
+
     this.removeEventListener("click", surSkyClick);
 
+    document.querySelector("#buzzLyd").play();
+
+    console.log(mist);
+
     document.querySelector("#mist").innerHTML = mist;
-
-
+    gameStatus();
+    tabtPoint();
 }
 
 function solClick() {
@@ -139,11 +262,16 @@ function solClick() {
     document.querySelector("#count").innerHTML = count;
 
     console.log(this);
-    this.classList.add("hide");
+    //this.classList.add("hide");
+    this.classList.remove("sol_ned");
 
     this.removeEventListener("click", solClick);
 
-    document.querySelector("#sol").classList.toggle("position" + Math.floor((Math.random() * 5) + 1));
+    document.querySelector("#jaLyd").play();
+
+
+    gameStatus();
+
 }
 
 function solbrillerClick() {
@@ -156,8 +284,14 @@ function solbrillerClick() {
     document.querySelector("#count").innerHTML = count;
 
     console.log(this);
-    this.classList.add("hide");
+    //this.classList.add("hide");
+
+    this.classList.remove("solbriller_ned");
     this.removeEventListener("click", solbrillerClick);
+    document.querySelector("#jaLyd").play();
+
+    gameStatus();
+
 }
 
 function isClick() {
@@ -170,21 +304,18 @@ function isClick() {
     document.querySelector("#count").innerHTML = count;
 
     console.log(this);
-    this.classList.add("hide");
+    //this.classList.add("hide");
+
+    this.classList.remove("is_ned");
 
     this.removeEventListener("click", isClick);
+    document.querySelector("#jaLyd").play();
 
-    document.querySelector("#is").classList.toggle("position" + Math.floor((Math.random() * 5) + 1));
-
+    gameStatus();
 }
 
-function nytElement() {
 
-    document.querySelector("#is").classList.toggle("position" + Math.floor((Math.random() * 5) + 1));
-
-
-    document.querySelector("#is").classList.remove("hide");
-}
+//          KODE FOR ELEMENTER DER FALDER NED SLUT!
 
 
 function timeLeftFc() {
@@ -192,8 +323,8 @@ function timeLeftFc() {
 
     if (timeLeft > 0) {
         timeLeft--;
-
-        setTimeout(timeLeftFc, 1000);
+        timeLeftTimeOut = setTimeout(timeLeftFc, 1000);
+        document.querySelector("#time").innerHTML = "00: " + timeLeft;
 
     } else {
         gameover();
@@ -203,31 +334,117 @@ function timeLeftFc() {
 function tabtLiv() {
     console.log("tabt liv");
 
-    if (mist < 1) {
+    if (mist == 0) {
         gameover();
 
         document.querySelector("#reset").addEventListener("click", tabtLivReset);
 
+    } else {
+        levelComplete();
     }
 }
 
+
+function gameStatus() {
+    console.log("gamestatus")
+    if (count == 10) {
+
+        levelComplete();
+    }
+}
+
+function tabtPoint() {
+    console.log("tabt Point");
+
+    if (count < 1) {
+        gameover();
+
+    }
+    document.querySelector("#reset").addEventListener("click", tabtLivReset);
+}
+
 function tabtLivReset() {
-    timeLeft = 5;
-    mist = 1;
+    mist = 3;
 }
 
 function gameover() {
-    console.log("spil slut");
+    console.log("spil tabt");
+
+    document.querySelector("#levelcomplete").classList.add("hide");
+
     document.querySelector("#gameover").classList.remove("hide");
     document.querySelector("#game_elements").classList.add("hide");
 
     document.querySelector("#reset").addEventListener("click", reset);
+    clearTimeout(timeLeftTimeOut);
+    timeLeft = 30;
+    document.querySelector("#time").innerHTML = "00:" + timeLeft;
+
+
+}
+
+function levelComplete() {
+    console.log("spil vundet");
+    document.querySelector("#reset1").addEventListener("click", reset);
+    document.querySelector("#gameover").classList.add("hide");
+
+    document.querySelector("#levelcomplete").classList.remove("hide");
+
+    document.querySelector("#game_elements").classList.add("hide");
+
+    clearTimeout(timeLeftTimeOut);
+    timeLeft = 30;
+    document.querySelector("#time").innerHTML = "00:" + timeLeft;
+
 
 }
 
 function reset() {
     document.querySelector("#gameover").classList.add("hide");
-    timeLeft = 5;
-    mist = 1;
-    showStart();
+    document.querySelector("#levelcomplete").classList.add("hide");
+
+    mist = 0;
+    count = 3;
+    //showStart();
+    window.history.go(0);
+}
+
+function reset1() {
+    document.querySelector("#gameover").classList.add("hide");
+    document.querySelector("#levelcomplete").classList.add("hide");
+
+    count = 0;
+    mist = 3;
+    //showStart();
+    window.history.go(0);
+}
+
+function toggleSounds() {
+
+    console.log("toggleSounds");
+    if (document.querySelector("#sfx_on").classList.contains("hide")) {
+        document.querySelector("#sfx_on").classList.remove("hide")
+        document.querySelector("#sfx_off").classList.add("hide")
+        document.querySelector("#buzzLyd").muted = false;
+        document.querySelector("#jaLyd").muted = false;
+    } else {
+        document.querySelector("#sfx_on").classList.add("hide")
+        document.querySelector("#sfx_off").classList.remove("hide")
+        document.querySelector("#buzzLyd").muted = true;
+        document.querySelector("#jaLyd").muted = true;
+    }
+}
+
+function toggleMusic() {
+
+    console.log("toggleMusic");
+    if (document.querySelector("#music_on").classList.contains("hide")) {
+        document.querySelector("#music_on").classList.remove("hide")
+        document.querySelector("#music_off").classList.add("hide")
+        document.querySelector("#musik").muted = false;
+    } else {
+        document.querySelector("#music_on").classList.add("hide")
+        document.querySelector("#music_off").classList.remove("hide")
+        document.querySelector("#musik").muted = true;
+    }
 }
